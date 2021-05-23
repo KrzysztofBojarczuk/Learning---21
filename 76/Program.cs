d282e19965381677afe9845cc627e7735d095d2c
KrzysztofBojarczuk <69596796+KrzysztofBojarczuk@users.noreply.github.com>
2021-05-23 10:16:38 +0200

    Add project files.

---
+++ 76/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _76
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tableka konwersji stopni Celsujsza na stopnie  Fahrenheita");
+            Console.WriteLine();
+            Pętla();
+
+        }
+        public static void Pętla()
+        {
+            Console.WriteLine("Stopnie Celsujsze             Stopnie Fahrenheita ");
+            Console.WriteLine("--------------------------------------------------");
+            for (int i = 0; i <= 20; i++)
+            {
+                double F = ((9.0 / 5.0) * i) + 32.0;
+                Console.WriteLine($"{i}                            {F.ToString("n")}");
+            }
+        }
+    }
+}
